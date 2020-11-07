3473ef6116e53631cce35b98cab758f3ac313a7a
Randall Cox <rcoxwork@gmail.com>
2020-11-06 16:25:34 -0800

    Add modal form validation

--- script.js
+++ script.js
@@ -16,3 +16,40 @@ function showModal() {
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Validate form
+function validate(nameValue, urlValue) {
+  const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+  if (!nameValue || !urlValue) {
+    alert('Please provide values for both fields.');
+    return false;
+  }
+  // if (urlValue.match(regex)) {
+  //   alert('match');
+  // }
+  if (!urlValue.match(regex)) {
+    alert('Please provide a valid web address');
+    return false;
+  }
+  // Valid
+  return true;
+}
+
+// Handle data from form
+function storeBookmark(e) {
+  e.preventDefault();
+  // console.log(e);
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes('http://', 'https://')) {
+    urlValue = `https://${urlValue}`;
+  }
+  // console.log(nameValue, urlValue);
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+}
+
+// Event listener
+bookmarkForm.addEventListener('submit', storeBookmark);
